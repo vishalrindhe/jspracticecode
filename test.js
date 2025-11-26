c7e98ebfbde710d2ee4244cb0131f3e26d004e7e
Vishal Rindhe <vishalv@smartyz.com>
2025-11-26 19:58:09 +0530

    new code

--- test.js
+++ test.js
@@ -1,27 +1,4 @@
-function check(val) {
-    if (val == null) {
-        console.log(false)
-        return false
-    }
-    const val2 = val.toString()
-    const val2len = val2.length
-    if (val2len <= 1) {
-        console.log(true);
-        return true;
-    }
-    for (let i = 0; i < val2len / 2 - 1; i++) {
-        if (val2[i] != val2[val2len - i - 1]) {
-            console.log(false)
-            return false;
-        }
-    }
-    console.log(true);
-    return true;
-}
-
-check(1234)
-check('abcd')
-check('abcdcba')
-check('a')
-check('')
-check(0)
+let a = [1, 2, 3, 4]
+let b = [5, 6, 7, 8]
+console.log([...a, ...b]);
+console.log([a, b].flatMap(a => a));
